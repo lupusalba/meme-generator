6230020c3c1db22edd94a0e10d3512cbe279bc95
lupusalba <a145296@gmail.com>
2022-06-22 20:33:00 +0200

    finished, all working

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -29,10 +29,14 @@ function handelChange(event) {
 const [allMemes, setAllMemes] = useState([])
 
   useEffect( () => {
-    console.log("effect ran")
-    fetch("https://api.imgflip.com/get_memes")
-      .then(res => res.json())
-      .then(data => setAllMemes(data.data.memes))
+    async function getMemes(){
+      const res = await fetch("https://api.imgflip.com/get_memes")
+      const data = await res.json()
+      setAllMemes(data.data.memes)
+    }
+    
+    getMemes()
+    
   }, [])
 
 
